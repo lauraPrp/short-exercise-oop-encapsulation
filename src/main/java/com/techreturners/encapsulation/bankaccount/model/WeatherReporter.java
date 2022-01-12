f6f1390acdd5b68a9d6fd1f58d66276d5a226675
lauraPrp <laura.porpiglia@gmail.com>
2022-01-12 14:59:41 +0000

    refactoring and test

--- src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
+++ src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
@@ -4,44 +4,48 @@ import java.text.MessageFormat;
 
 public class WeatherReporter {
 
-    public String location;
-    public double temperature;
+    private String location;
+    private double temperature;
+
+    public final String LONDON = "London";
+    public final String CALIFORNIA = "California";
+    public final String CAPE_TOWN = "Cape Town";
 
     public WeatherReporter(String location, double temperature) {
         this.location = location;
         this.temperature = temperature;
     }
 
-    public String print() {
+    private double convertCelsiusToFahreneit(double temp) {
+        final double CTF = 1.8;
+        final int CELSIUS_TO_FAHRENHEIT = 32;
+        return CTF * temp + CELSIUS_TO_FAHRENHEIT;
+    }
 
-        double newTemp = (9.0 / 5.0) * temperature + 32;
-        return MessageFormat.format("I am in {0} and it is {1}. {2}. The temperature in Fahrenheit is {3}.", location, check1(), check2(), newTemp);
+
+    public String print() {
+        return MessageFormat.format("I am in {0} and it is {1}. {2} The temperature in Fahrenheit is {3}.",
+                location, check1(), check2(), convertCelsiusToFahreneit(temperature));
 
     }
 
     public String check1() {
-        if (location == "London") {
-
-            return "🌦";
-
-        } else if (location == "California") {
-
-            return "🌅";
-
-        } else if (location == "Cape Town") {
-
-            return "🌤";
-
-        }
-        return "🔆";
+        return switch (location) {
+            case LONDON -> "🌦";
+            case CALIFORNIA -> "🌅";
+            case CAPE_TOWN -> "🌤";
+            default -> "🔆";
+        };
     }
 
     public String check2() {
-        if (temperature > 30) {
+        int HOT_TEMP = 30;
+        int COLD_TEMP = 10;
+        if (temperature > HOT_TEMP) {
 
             return "It's too hot 🥵!";
 
-        } else if (temperature < 10) {
+        } else if (temperature < COLD_TEMP) {
 
             return "It's too cold 🥶!";
 
---
+++ src/test/java/com/techreturners/encapsulation/bankaccount/WeatherReporterTest.java
@@ -0,0 +1,56 @@
+package com.techreturners.encapsulation.bankaccount;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.techreturners.encapsulation.bankaccount.model.WeatherReporter;
+import org.junit.Test;
+
+public class WeatherReporterTest {
+
+    @Test
+    public void checkColdWeather() {
+        WeatherReporter wr = new WeatherReporter("London",4.0);
+        WeatherReporter wr2 = new WeatherReporter("California",-2.0);
+        WeatherReporter wr3 = new WeatherReporter("Cape Town",0.0);
+        assertEquals("I am in London and it is 🌦. It's too cold 🥶! The temperature in Fahrenheit is 39,2.",wr.print());
+        assertEquals("I am in California and it is 🌅. It's too cold 🥶! The temperature in Fahrenheit is 28,4.",wr2.print());
+        assertEquals("I am in Cape Town and it is 🌤. It's too cold 🥶! The temperature in Fahrenheit is 32.",wr3.print());
+
+
+        }
+    @Test
+    public void checkHotWeather() {
+        WeatherReporter wr = new WeatherReporter("London",35);
+        WeatherReporter wr2 = new WeatherReporter("California",40);
+        WeatherReporter wr3 = new WeatherReporter("Cape Town",38);
+        assertEquals("I am in London and it is 🌦. It's too hot 🥵! The temperature in Fahrenheit is 95.",wr.print());
+        assertEquals("I am in California and it is 🌅. It's too hot 🥵! The temperature in Fahrenheit is 104.",wr2.print());
+        assertEquals("I am in Cape Town and it is 🌤. It's too hot 🥵! The temperature in Fahrenheit is 100,4.",wr3.print());
+
+    }
+    @Test
+    public void checkRightWeather() {
+        WeatherReporter wr = new WeatherReporter("London",15);
+        WeatherReporter wr2 = new WeatherReporter("California",20);
+        WeatherReporter wr3 = new WeatherReporter("Cape Town",18);
+
+        assertEquals("I am in London and it is 🌦. Ahhh...it's just right 😊! The temperature in Fahrenheit is 59.",wr.print());
+        assertEquals("I am in California and it is 🌅. Ahhh...it's just right 😊! The temperature in Fahrenheit is 68.",wr2.print());
+        assertEquals("I am in Cape Town and it is 🌤. Ahhh...it's just right 😊! The temperature in Fahrenheit is 64,4.",wr3.print());
+
+
+    }
+    @Test
+    public void checkExtremeWeather() {
+        WeatherReporter wr = new WeatherReporter("London",Double.MIN_VALUE);
+        WeatherReporter wr2 = new WeatherReporter("California",Double.MAX_VALUE+2);
+        WeatherReporter wr3 = new WeatherReporter("Cape Town",Double.NEGATIVE_INFINITY-3.0);
+
+        assertEquals("I am in London and it is 🌦. It's too cold 🥶! The temperature in Fahrenheit is 32.",wr.print());
+        assertEquals("I am in California and it is 🌅. It's too hot 🥵! The temperature in Fahrenheit is ∞.",wr2.print());
+        assertEquals("I am in Cape Town and it is 🌤. It's too cold 🥶! The temperature in Fahrenheit is -∞.",wr3.print());
+
+    }
+}
